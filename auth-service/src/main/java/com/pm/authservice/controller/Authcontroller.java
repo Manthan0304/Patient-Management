1477d822e1c063b36ccecfa2f91c62acf9ba1db4
Manthan Singla <manthansingla786@gmail.com>
2026-02-23 19:16:57 +0530

    auth Service Added issue - api gateway

---
+++ auth-service/src/main/java/com/pm/authservice/controller/Authcontroller.java
@@ -0,0 +1,65 @@
+package com.pm.authservice.controller;
+
+import com.pm.authservice.dto.LoginRequestDTO;
+import com.pm.authservice.dto.LoginResponseDTO;
+import com.pm.authservice.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+public class Authcontroller {
+    private final AuthService authService;
+
+    public Authcontroller(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @Operation(summary = "Genarate token on User Login")
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequestDTO) {
+
+        Optional<String> tokenoptional = authService.authenticate(loginRequestDTO);
+        if (tokenoptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        String token = tokenoptional.get();
+
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+
+    }
+
+    @Operation(summary = "Validate Token")
+    @GetMapping("/validate")
+    public ResponseEntity<Void> validateToken(@RequestHeader("Authorization") String authHeader) {
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        return authService.validateToken(authHeader.substring(7))
+                ? ResponseEntity.ok().build()
+                : ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
